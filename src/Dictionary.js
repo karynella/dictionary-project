2931e0bab4bc884398a5e45f83f30baf6770bca1
karynella <karynella@live.com>
2022-02-06 19:28:21 +0000

    added content from API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results.js";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response);
+    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -30,6 +33,7 @@ export default function Dictionary() {
           autoFocus={true}
         />
       </form>
+      <Results results={results} />
     </div>
   );
 }
